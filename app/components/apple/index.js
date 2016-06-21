480e939fc89fae3325faf09f014c0aa10b8b478e
zhishui <zhi_shui@hunliji.com>
2016-06-21 18:55:34 +0800

    transform-class-properties

--- app/components/apple/index.js
+++ app/components/apple/index.js
@@ -1,10 +1,23 @@
 import React,{Component,PropTypes} from 'react'
 import styles from './style.css'
 export default class extends Component{
+    // static propTypes={
+    //     handler:PropTypes.func
+    // }
+    state={
+        value:'双向绑定'
+    }
+    changeHandler(e){
+        this.setState({
+            value:e.target.value
+        })
+    }
     render(){
+        let value=this.state.value
         return(
             <div>
-                Apple
+                <h1>{value||'双向绑定'}</h1>
+                <input type="text" onChange={::this.changeHandler} defaultValue={value}/>
             </div>
         )
     }
