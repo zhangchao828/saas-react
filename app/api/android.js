a51a0fe5aa8e9a9e14d0d999bc37afa9c1a0b2e9
zhishui <zhi_shui@hunliji.com>
2016-06-25 14:01:57 +0800

    fetch

--- app/api/android.js
+++ app/api/android.js
@@ -5,7 +5,12 @@ export default{
             let res= await fetch('/assets/data/test.json',{
                 method:'GET',
                 //body:JSON.stringify(params)
-            });
+            })
+            /*
+            不要试图打印res.json(),因为这样就已经执行了res.json()，
+            它是一个Promise对象，然后下面又return一下res.json(),又执行一遍就会报错
+             */
+            console.log(res.json())
             return res.json();
         }catch (err){
             console.log(err)
