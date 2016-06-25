636a839871e8839a108679c42dddee7ef17299e8
zhishui <zhi_shui@hunliji.com>
2016-06-25 19:14:28 +0800

    fetch

--- app/actions/androidAction.js
+++ app/actions/androidAction.js
@@ -3,14 +3,23 @@ import API from '../api/android'
 export const search=params=>async (dispatch,getState)=>{
     try {
         let response=await API.search_api(params)
-        let result =await response.json()
-        dispatch({
-            type:type.SEARCH,
-            data:result.data
-        })
-        setTimeout(()=>{
-            dispatch(loading(false))
-        },1500)
+        //请求成功之后，ok为true，status为200
+        if(response.ok){
+            let result =await response.json()
+            dispatch({
+                type:type.SEARCH,
+                data:result.data
+            })
+            setTimeout(()=>{
+                dispatch(loading(false))
+            },1500)
+        }else{
+            // dispatch({
+            //     type:'ERROR',
+            //     errInfo:response.statusText
+            // })
+            console.log(`状态码:${response.status},错误信息:${response.statusText}`)
+        }
     }catch (err){
         console.log(err)
     }
--- app/api/android.js
+++ app/api/android.js
@@ -1,6 +1,9 @@
 import fetch from './fetch'
 export default{
     search_api:params=>{
-        return fetch('/assets/data/test.json')
+        return fetch('/assets/data/test.json',{
+            // method:'GET',
+            // body:JSON.stringify(params)
+        })
     }
 }
